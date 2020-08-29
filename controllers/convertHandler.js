cddf2142ec529a5d685aa344610d0909254b8a95
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-29 16:49:06 +0100

    Added code to return a json response of the conversion

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -14,12 +14,21 @@ function ConvertHandler() {
   };
   
   this.getUnit = function(input) {
-    var result = input.match(/[a-zA-Z\/]+$/);    
+    var result = input.match(/[a-zA-Z\/]+$/)[0];    
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     var result;
+
+    switch(initUnit.toLowerCase()){
+      case 'gal': result = 'l'; break;
+      case 'lbs': result = 'kg'; break;
+      case 'mi': result = 'km'; break;
+      case 'l': result = 'gal'; break;
+      case 'kg': result = 'lbs'; break;
+      case 'km': result = 'mi'; break;
+    }    
     
     return result;
   };
@@ -35,12 +44,25 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     var result;
+
+    switch(initUnit){
+      case 'gal':
+        result = initNum * galToL;
+        break;
+      case 'mi':
+	result = initNum * miToKm;        
+        break;
+      case 'lbs':
+        result = initNum * lbsToKg;
+        break;
+
+    }
     
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
+    var result = {initNum, initUnit, returnNum, returnUnit};
     
     return result;
   };
